2adb81df5c088af6dd8d40e153a279813a913a82
Michael Miller <icy.arctic.fox@gmail.com>
2018-04-18 20:17:20 -0600

    Friendlier method names

--- GLFW.Net/Native/GLFW.Context.cs
+++ GLFW.Net/Native/GLFW.Context.cs
@@ -8,8 +8,8 @@ namespace GLFW.Net.Native
         /// <summary>
         /// Client API function pointer type.
         /// </summary>
-        /// <seealso cref="GLFW.glfwGetProcAddress"/>
-        public delegate void GlProc();
+        /// <seealso cref="GetProcAddress"/>
+        public delegate void Proc();
 
         /// <summary>
         /// Makes the context of the specified window current for the calling thread.
@@ -27,9 +27,9 @@ namespace GLFW.Net.Native
         /// </summary>
         /// <param name="window">The window whose context to make current,
         /// or <c>null</c> to detach the current context.</param>
-        /// <seealso cref="glfwGetCurrentContext"/>
-        [DllImport(DllName)]
-        public static extern void glfwMakeContextCurrent(IntPtr window);
+        /// <seealso cref="GetCurrentContext"/>
+        [DllImport(DllName, EntryPoint = "glfwMakeContextCurrent")]
+        public static extern void MakeContextCurrent(IntPtr window);
 
         /// <summary>
         /// Returns the window whose context is current on the calling thread.
@@ -39,20 +39,20 @@ namespace GLFW.Net.Native
         /// </summary>
         /// <returns>The window whose context is current,
         /// or <c>null</c> if no window's context is current.</returns>
-        /// <seealso cref="glfwMakeContextCurrent"/>
-        [DllImport(DllName)]
-        public static extern IntPtr glfwGetCurrentContext();
+        /// <seealso cref="MakeContextCurrent"/>
+        [DllImport(DllName, EntryPoint = "glfwGetCurrentContext")]
+        public static extern IntPtr GetCurrentContext();
 
         /// <summary>
         /// Sets the swap interval for the current context.
         /// <para>This function sets the swap interval for the current OpenGL or OpenGL ES context,
-        /// i.e. the number of screen updates to wait from the time <see cref="glfwSwapBuffers"/>
+        /// i.e. the number of screen updates to wait from the time <see cref="SwapBuffers"/>
         /// was called before swapping the buffers and returning.
         /// This is sometimes called vertical synchronization, vertical retrace synchronization or just vsync.</para>
         /// <para>Contexts that support either of the <c>WGL_EXT_swap_control_tear</c>
         /// and <c>GLX_EXT_swap_control_tear</c> extensions also accept negative swap intervals,
         /// which allow the driver to swap even if a frame arrives a little bit late.
-        /// You can check for the presence of these extensions using <see cref="glfwExtensionSupported"/>.
+        /// You can check for the presence of these extensions using <see cref="ExtensionSupported"/>.
         /// For more information about swap tearing, see the extension specifications.</para>
         /// <para>A context must be current on the calling thread.
         /// Calling this function without a current context will cause a <see cref="NoCurrentContext"/> error.</para>
@@ -62,7 +62,7 @@ namespace GLFW.Net.Native
         /// <see cref="NotInitialized"/>, <see cref="NoCurrentContext"/>, and <see cref="PlatformError"/>.</para>
         /// </summary>
         /// <param name="interval">The minimum number of screen updates to wait for
-        /// until the buffers are swapped by <see cref="glfwSwapBuffers"/>.</param>
+        /// until the buffers are swapped by <see cref="SwapBuffers"/>.</param>
         /// <remarks>
         /// <para>This function is not called during context creation,
         /// leaving the swap interval set to whatever is the default on that platform.
@@ -72,9 +72,9 @@ namespace GLFW.Net.Native
         /// either because of a user setting that overrides the application's request
         /// or due to bugs in the driver.</para>
         /// </remarks>
-        /// <seealso cref="glfwSwapBuffers"/>
-        [DllImport(DllName)]
-        public static extern void glfwSwapInterval(int interval);
+        /// <seealso cref="SwapBuffers"/>
+        [DllImport(DllName, EntryPoint = "glfwSwapInterval")]
+        public static extern void SwapInterval(int interval);
 
         /// <summary>
         /// Returns whether the specified extension is available.
@@ -88,7 +88,7 @@ namespace GLFW.Net.Native
         /// The extension strings will not change during the lifetime of a context,
         /// so there is no danger in doing this.</para>
         /// <para>This function does not apply to Vulkan.
-        /// If you are using Vulkan, see <see cref="glfwGetRequiredInstanceExtensions"/>,
+        /// If you are using Vulkan, see <see cref="GetRequiredInstanceExtensions"/>,
         /// <c>vkEnumerateInstanceExtensionProperties</c>
         /// and <c>vkEnumerateDeviceExtensionProperties</c> instead.</para>
         /// <para>Possible errors include
@@ -97,9 +97,9 @@ namespace GLFW.Net.Native
         /// </summary>
         /// <param name="extension">The ASCII encoded name of the extension.</param>
         /// <returns><see cref="True"/> if the extension is available, or <see cref="False"/> otherwise.</returns>
-        /// <seealso cref="glfwGetProcAddress"/>
-        [DllImport(DllName)]
-        public static extern int glfwExtensionSupported([MarshalAs(UnmanagedType.LPStr)] string extension);
+        /// <seealso cref="GetProcAddress"/>
+        [DllImport(DllName, EntryPoint = "glfwExtensionSupported")]
+        public static extern int ExtensionSupported([MarshalAs(UnmanagedType.LPStr)] string extension);
 
         /// <summary>
         /// Returns the address of the specified function for the current context.
@@ -108,7 +108,7 @@ namespace GLFW.Net.Native
         /// <para>A context must be current on the calling thread.
         /// Calling this function without a current context will cause a <see cref="NoCurrentContext"/> error.</para>
         /// <para>This function does not apply to Vulkan.
-        /// If you are rendering with Vulkan, see <see cref="glfwGetInstanceProcAddress"/>,
+        /// If you are rendering with Vulkan, see <see cref="GetInstanceProcAddress"/>,
         /// <c>vkGetInstanceProcAddr</c> and <c>vkGetDeviceProcAddr</c> instead.</para>
         /// <para>Possible errors include
         /// <see cref="NotInitialized"/>, <see cref="NoCurrentContext"/>, and <see cref="PlatformError"/>.</para>
@@ -121,9 +121,9 @@ namespace GLFW.Net.Native
         /// despite the associated version or extension not being available.
         /// Always check the context version or extension string first.</para>
         /// </remarks>
-        /// <seealso cref="glfwExtensionSupported"/>
-        [DllImport(DllName)]
+        /// <seealso cref="ExtensionSupported"/>
+        [DllImport(DllName, EntryPoint = "glfwGetProcAddress")]
         [return: MarshalAs(UnmanagedType.FunctionPtr)]
-        public static extern GlProc glfwGetProcAddress(string procName);
+        public static extern Proc GetProcAddress([MarshalAs(UnmanagedType.LPStr)] string procName);
     }
 }
