a029be5b7564ad9bd7c3b69a83b66ec4f9035392
Michael Miller <icy.arctic.fox@gmail.com>
2018-04-21 17:13:18 -0600

    Add methods for converting to and from UTF-8 native strings
    Very strange that .NET doesn't have this built-in, unless I'm missing something.

---
+++ GLFW.Net/Utf8Marshal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GLFW.Net
+{
+    public static class Utf8Marshal
+    {
+        /// <summary>
+        /// Reads a UTF-8 null-terminated string from a native pointer.
+        /// The string will be copied so that managed memory can take ownership of it.
+        /// </summary>
+        /// <param name="nativeString">Pointer to an unmanaged string containing the UTF-8 string.</param>
+        /// <returns>Managed string copied from unmanaged memory.</returns>
+        public static string FromNativeUtf8(this IntPtr nativeString)
+        {
+            // Find out how long the string is.
+            // It will be null-terminated, so look for that.
+            var length = 0;
+            while (Marshal.ReadByte(nativeString, length) != '\0')
+                length++;
+            
+            // Allocate managed memory and copy the string into it.
+            var buffer = new byte[length];
+            Marshal.Copy(nativeString, buffer, 0, length);
+            
+            // Convert the UTF-8 string into the native format.
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        /// <summary>
+        /// Creates a null-terminated UTF-8 string that can be used in unmanaged code.
+        /// The string will be copied and the GC will not cleanup the unmanaged copy.
+        /// </summary>
+        /// <param name="managedString">Managed string to copy to unmanaged memory.</param>
+        /// <returns>Pointer to an unmanaged string containing the UTF-8 string.</returns>
+        public static IntPtr ToNativeUtf8(this string managedString)
+        {
+            // Get the length of the string as it would appear in UTF-8 and allocate memory for that amount.
+            var length = Encoding.UTF8.GetByteCount(managedString);
+            var buffer = new byte[length + 1]; // +1 to leave space for the null-terminator.
+
+            // Encode the string in UTF-8.
+            Encoding.UTF8.GetBytes(managedString, 0, managedString.Length, buffer, length);
+            
+            // Allocate unmanaged memory and copy the buffer to it.
+            var nativeString = Marshal.AllocHGlobal(buffer.Length);
+            Marshal.Copy(buffer, 0, nativeString, buffer.Length);
+            
+            return nativeString;
+        }
+    }
+}
