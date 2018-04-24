8de9a0f5424edf12d3505a2082477f5d7c87ddb1
Michael Miller <icy.arctic.fox@gmail.com>
2018-04-23 18:31:20 -0600

    Add method stubs for Monitor struct

---
+++ GLFW.Net/Monitor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GLFW.Net
+{
+    public struct Monitor
+    {
+        private IntPtr _monitorPointer;
+
+        private Monitor(IntPtr monitorPointer)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public static Monitor[] List
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public static Monitor Primary
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object Position
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object Size
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object PhysicalSize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object[] VideoModes
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object VideoMode
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object GammaRamp
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public void SetGamma(float gamma)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
