26f0d7e9e7ed1b96653ea069e28ba9372d1d6e64
Michael Miller <icy.arctic.fox@gmail.com>
2018-05-02 16:37:27 -0600

    Custom marshal methods for monitor methods

--- GLFW.Net/GLFW.Monitor.cs
+++ GLFW.Net/GLFW.Monitor.cs
@@ -6,28 +6,215 @@ namespace GLFW.Net
 {
     public static partial class GLFW
     {
+        /// <summary>
+        /// The function signature for monitor configuration callbacks.
+        /// </summary>
+        /// <param name="monitor">The monitor that was connected or disconnected.</param>
+        /// <param name="event">One of <see cref="DeviceEvent.Connected"/>
+        /// or <see cref="DeviceEvent.Disconnected"/></param>
+        /// <seealso cref="SetMonitorCallback"/>
+        internal delegate void MonitorCallback(IntPtr monitor, DeviceEvent @event);
+
+        /// <summary>
+        /// Returns the currently connected monitors.
+        /// <para>This function returns an array of handles for all currently connected monitors.
+        /// The primary monitor is always first in the returned array.
+        /// If no monitors were found, this function returns <c>null</c>.</para>
+        /// </summary>
+        /// <returns>An array of monitor handles, or an empty array if no monitors were found.</returns>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        /// <seealso cref="GetPrimaryMonitor"/>
+        internal static IntPtr[] GetMonitors()
+        {
+            var arrayPointer = Internal.GetMonitors(out var count);
+            HandleError();
+            if (arrayPointer == IntPtr.Zero)
+                return new IntPtr[0];
+            var monitors = new IntPtr[count];
+            Marshal.Copy(arrayPointer, monitors, 0, count);
+            return monitors;
+        }
+
+        /// <summary>
+        /// Returns the primary monitor.
+        /// <para>This function returns the primary monitor.
+        /// This is usually the monitor where elements like the task bar or global menu bar are located.</para>
+        /// </summary>
+        /// <returns>The primary monitor, or <c>null</c> if no monitors were found.</returns>
+        /// <remarks>The primary monitor is always first in the array returned by <see cref="GetMonitors"/>.</remarks>
+        /// <seealso cref="GetMonitors"/>
+        internal static IntPtr GetPrimaryMonitor()
+        {
+            var monitorPointer = Internal.GetPrimaryMonitor();
+            HandleError();
+            return monitorPointer;
+        }
+
+        /// <summary>
+        /// Returns the position of the monitor's viewport on the virtual screen.
+        /// <para>This function returns the position, in screen coordinates,
+        /// of the upper-left corner of the specified monitor.</para>
+        /// </summary>
+        /// <param name="monitor">The monitor to query.</param>
+        /// <param name="xpos">Where to store the monitor x-coordinate.</param>
+        /// <param name="ypos">Where to store the monitor y-coordinate.</param>
+        /// <exception cref="PlatformErrorGLFWException">This operation is not supported on this platform.</exception>
+        internal static void GetMonitorPos(IntPtr monitor, out int xpos, out int ypos)
+        {
+            Internal.GetMonitorPos(monitor, out xpos, out ypos);
+            HandleError();
+        }
+
+        /// <summary>
+        /// Returns the physical size of the monitor.
+        /// <para>This function returns the size, in millimetres, of the display area of the specified monitor.</para>
+        /// <para>Some systems do not provide accurate monitor size information,
+        /// either because the monitor EDID data is incorrect
+        /// or because the driver does not report it accurately.</para>
+        /// </summary>
+        /// <param name="monitor">The monitor to query.</param>
+        /// <param name="widthMM">Where to store the width, in millimetres, of the monitor's display area.</param>
+        /// <param name="heightMM">	Where to store the height, in millimetres, of the monitor's display area.</param>
+        /// <remarks><para>Windows: calculates the returned physical size from the current resolution and system DPI
+        /// instead of querying the monitor EDID data.</para></remarks>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        internal static void GetMonitorPhysicalSize(IntPtr monitor, out int widthMM, out int heightMM)
+        {
+            Internal.GetMonitorPhysicalSize(monitor, out widthMM, out heightMM);
+            HandleError();
+        }
+
+        /// <summary>
+        /// Returns the name of the specified monitor.
+        /// <para>This function returns a human-readable name, encoded as UTF-8, of the specified monitor.
+        /// The name typically reflects the make and model of the monitor
+        /// and is not guaranteed to be unique among the connected monitors.</para>
+        /// </summary>
+        /// <param name="monitor">The monitor to query.</param>
+        /// <returns>The UTF-8 encoded name of the monitor.</returns>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        internal static string GetMonitorName(IntPtr monitor)
+        {
+            var namePointer = Internal.GetMonitorName(monitor);
+            HandleError();
+            return namePointer.FromNativeUtf8();
+        }
+
+        /// <summary>
+        /// Sets the monitor configuration callback.
+        /// <para>This function sets the monitor configuration callback, or removes the currently set callback.
+        /// This is called when a monitor is connected to or disconnected from the system.</para>
+        /// </summary>
+        /// <param name="callback">The new callback, or <c>null</c> to remove the currently set callback.</param>
+        /// <returns>The previously set callback,
+        /// or <c>null</c> if no callback was set or the library had not been initialized.</returns>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        internal static MonitorCallback SetMonitorCallback(MonitorCallback callback)
+        {
+            return SetInternalCallback(Internal.SetMonitorCallback, callback);
+        }
+
+        /// <summary>
+        /// Returns the available video modes for the specified monitor.
+        /// <para>This function returns an array of all video modes supported by the specified monitor.
+        /// The returned array is sorted in ascending order,
+        /// first by color bit depth (the sum of all channel depths)
+        /// and then by resolution area (the product of width and height).</para>
+        /// </summary>
+        /// <param name="monitor">The monitor to query.</param>
+        /// <returns>An array of video modes.</returns>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        /// <exception cref="PlatformErrorGLFWException">This operation is not supported on this platform.</exception>
+        /// <seealso cref="GetVideoMode"/>
+        internal static IntPtr[] GetVideoModes(IntPtr monitor)
+        {
+            var modesPointer = Internal.GetVideoModes(monitor, out var count);
+            HandleError();
+            var modes = new IntPtr[count];
+            Marshal.Copy(modesPointer, modes, 0, count);
+            return modes;
+        }
+
+        /// <summary>
+        /// Returns the current mode of the specified monitor.
+        /// <para>This function returns the current video mode of the specified monitor.
+        /// If you have created a full screen window for that monitor,
+        /// the return value will depend on whether that window is iconified.</para>
+        /// </summary>
+        /// <param name="monitor">The monitor to query.</param>
+        /// <returns>The current mode of the monitor.</returns>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        /// <exception cref="PlatformErrorGLFWException">This operation is not supported on this platform.</exception>
+        /// <seealso cref="GetVideoModes"/>
+        internal static IntPtr GetVideoMode(IntPtr monitor)
+        {
+            var modePointer = Internal.GetVideoMode(monitor);
+            HandleError();
+            return modePointer;
+        }
+
+        /// <summary>
+        /// Generates a gamma ramp and sets it for the specified monitor.
+        /// <para>This function generates a 256-element gamma ramp
+        /// from the specified exponent and then calls <see cref="SetGammaRamp"/> with it.
+        /// The value must be a finite number greater than zero.</para>
+        /// </summary>
+        /// <param name="monitor">The monitor whose gamma ramp to set.</param>
+        /// <param name="gamma">The desired exponent.</param>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        /// <exception cref="PlatformErrorGLFWException">This operation is not supported on this platform.</exception>
+        /// <exception cref="InvalidValueGLFWException">The <paramref name="gamma"/> value is invalid.</exception>
+        internal static void SetGamma(IntPtr monitor, float gamma)
+        {
+            Internal.SetGamma(monitor, gamma);
+            HandleError();
+        }
+
+        /// <summary>
+        /// Returns the current gamma ramp for the specified monitor.
+        /// </summary>
+        /// <param name="monitor">The monitor to query.</param>
+        /// <returns>The current gamma ramp.</returns>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        /// <exception cref="PlatformErrorGLFWException">This operation is not supported on this platform.</exception>
+        internal static IntPtr GetGammaRamp(IntPtr monitor)
+        {
+            var rampPointer = Internal.GetGammaRamp(monitor);
+            HandleError();
+            return rampPointer;
+        }
+
+        /// <summary>
+        /// Sets the current gamma ramp for the specified monitor.
+        /// <para>This function sets the current gamma ramp for the specified monitor.
+        /// The original gamma ramp for that monitor is saved by GLFW the first time this function is called
+        /// and is restored by <see cref="Terminate"/>.</para>
+        /// </summary>
+        /// <param name="monitor">The monitor whose gamma ramp to set.</param>
+        /// <param name="ramp">The gamma ramp to use.</param>
+        /// <remarks><para>Gamma ramp sizes other than 256
+        /// are not supported by all platforms or graphics hardware.</para>
+        /// <para>Windows: The gamma ramp size must be 256.</para></remarks>
+        /// <exception cref="NotInitializedGLFWException">GLFW is not initialized.</exception>
+        /// <exception cref="PlatformErrorGLFWException">This operation is not supported on this platform.</exception>
+        internal static void SetGammaRamp(IntPtr monitor, IntPtr ramp)
+        {
+            Internal.SetGammaRamp(monitor, ramp);
+            HandleError();
+        }
+        
         private static partial class Internal
         {
-            /// <summary>
-            /// The function signature for monitor configuration callbacks.
-            /// </summary>
-            /// <param name="monitor">The monitor that was connected or disconnected.</param>
-            /// <param name="event">One of <see cref="DeviceEvent.Connected"/>
-            /// or <see cref="DeviceEvent.Disconnected"/></param>
-            /// <seealso cref="SetMonitorCallback"/>
-            public delegate void MonitorCallback(IntPtr monitor, DeviceEvent @event);
-
             /// <summary>
             /// Returns the currently connected monitors.
             /// <para>This function returns an array of handles for all currently connected monitors.
             /// The primary monitor is always first in the returned array.
-            /// If no monitors were found, this function returns <c>null</c>.</para>
+            /// If no monitors were found, this function returns <see cref="IntPtr.Zero"/>.</para>
             /// </summary>
             /// <param name="count">Where to store the number of monitors in the returned array.
             /// This is set to zero if an error occurred.</param>
-            /// <returns>An array of monitor handles,
-            /// or <c>null</c> if no monitors were found or if an error occurred.</returns>
-            /// <remarks>Possible errors include <see cref="ErrorCode.NotInitialized"/>.</remarks>
+            /// <returns>An array of monitor handles, 
+            /// or <see cref="IntPtr.Zero"/> if no monitors were found or if an error occurred.</returns>
             /// <seealso cref="GetPrimaryMonitor"/>
             [SuppressUnmanagedCodeSecurity]
             [DllImport(DllName, EntryPoint = "glfwGetMonitors", CallingConvention = CallingConvention.Cdecl)]
@@ -38,7 +225,8 @@ namespace GLFW.Net
             /// <para>This function returns the primary monitor.
             /// This is usually the monitor where elements like the task bar or global menu bar are located.</para>
             /// </summary>
-            /// <returns>The primary monitor, or <c>null</c> if no monitors were found or if an error occurred.</returns>
+            /// <returns>The primary monitor,
+            /// or <see cref="IntPtr.Zero"/> if no monitors were found or if an error occurred.</returns>
             /// <remarks><para>Possible errors include <see cref="ErrorCode.NotInitialized"/>.</para>
             /// <para>The primary monitor is always first in the array
             /// returned by <see cref="GetMonitors"/>.</para></remarks>
@@ -96,15 +284,14 @@ namespace GLFW.Net
             /// <para>This function sets the monitor configuration callback, or removes the currently set callback.
             /// This is called when a monitor is connected to or disconnected from the system.</para>
             /// </summary>
-            /// <param name="cbfun">The new callback, or <c>null</c> to remove the currently set callback.</param>
+            /// <param name="cbfun">The new callback,
+            /// or <see cref="IntPtr.Zero"/> to remove the currently set callback.</param>
             /// <returns>The previously set callback,
-            /// or <c>null</c> if no callback was set or the library had not been initialized.</returns>
+            /// or <see cref="IntPtr.Zero"/> if no callback was set or the library had not been initialized.</returns>
             /// <remarks>Possible errors include <see cref="ErrorCode.NotInitialized"/>.</remarks>
             [SuppressUnmanagedCodeSecurity]
             [DllImport(DllName, EntryPoint = "glfwSetMonitorCallback", CallingConvention = CallingConvention.Cdecl)]
-            [return: MarshalAs(UnmanagedType.FunctionPtr)]
-            public static extern MonitorCallback SetMonitorCallback(
-                [MarshalAs(UnmanagedType.FunctionPtr)] MonitorCallback cbfun);
+            public static extern IntPtr SetMonitorCallback(IntPtr cbfun);
 
             /// <summary>
             /// Returns the available video modes for the specified monitor.
