85b3e5296721852f3f520305e04694ddbe3808cb
Michael Miller <icy.arctic.fox@gmail.com>
2018-05-06 14:54:27 -0600

    Add gamma ramp structures

---
+++ GLFW.Net/UnmanagedGammaRamp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GLFW.Net
+{
+    /// <summary>
+    /// Describes the gamma ramp of a monitor.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct UnmanagedGammaRamp
+    {
+        /// <summary>
+        /// Pointer to an array describing the response of the red channel.
+        /// </summary>
+        public IntPtr Red;
+
+        /// <summary>
+        /// Pointer to an array describing the response of the green channel.
+        /// </summary>
+        public IntPtr Green;
+
+        /// <summary>
+        /// Pointer to an array describing the response of the blue channel.
+        /// </summary>
+        public IntPtr Blue;
+
+        /// <summary>
+        /// Number of items in each array.
+        /// </summary>
+        public uint Size;
+    }
+}
